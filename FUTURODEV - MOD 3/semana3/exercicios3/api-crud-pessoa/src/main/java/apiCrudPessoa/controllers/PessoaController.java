13dbd42f87ac7d6cd0e6cbb470d6bfab047bf221
luizfelippeantunes <@NTUNEs31101989>
2022-10-29 15:38:28 -0300

    M3S03EX4

--- FUTURODEV - MOD 3/semana3/exercicios3/api-crud-pessoa/src/main/java/apiCrudPessoa/controllers/PessoaController.java
+++ FUTURODEV - MOD 3/semana3/exercicios3/api-crud-pessoa/src/main/java/apiCrudPessoa/controllers/PessoaController.java
@@ -2,22 +2,26 @@ package apiCrudPessoa.controllers;
 
 import apiCrudPessoa.dto.UsuarioRM;
 import apiCrudPessoa.input.UsuarioInput;
+import apiCrudPessoa.models.Pessoa;
 import apiCrudPessoa.models.UsuarioModel;
+import apiCrudPessoa.repositories.PessoaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/pessoa")
 public class PessoaController {
 
-    /*@PostMapping(value = "/", produces = "application/json")
-    public ResponseEntity<UsuarioRM> cadastrar(@RequestBody UsuarioInput usuarioInput) {
-        UsuarioModel user = cadastroUsuarioService.salvar(toDomainObject(usuarioInput));
-        return new ResponseEntity<UsuarioRM>(toModel(user), HttpStatus.CREATED);
-    }*/
+    private PessoaRepository pessoaRepository;
 
+    @PostMapping(value = "/", produces = "application/json")
+    public Pessoa cadastrar(@RequestBody Pessoa pessoa) {
+        return pessoaRepository.save(pessoa);
+    }
+
+    @GetMapping(value = "/{idPessoa}", produces = "application/json")
+    public Pessoa buscar(@PathVariable(value = "idPessoa") Long idPessoa) {
+       return pessoaRepository.findById(idPessoa).get();
+    }
 }
