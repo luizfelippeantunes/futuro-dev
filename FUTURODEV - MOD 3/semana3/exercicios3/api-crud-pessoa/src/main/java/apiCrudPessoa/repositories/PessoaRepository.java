64773e20dffa126efd2d5a63bde558c3674ee89f
luizfelippeantunes <@NTUNEs31101989>
2022-10-29 15:17:26 -0300

    M3S03EX2

---
+++ FUTURODEV - MOD 3/semana3/exercicios3/api-crud-pessoa/src/main/java/apiCrudPessoa/repositories/PessoaRepository.java
@@ -0,0 +1,16 @@
+package apiCrudPessoa.repositories;
+
+import apiCrudPessoa.models.Pessoa;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface PessoaRepository  extends CrudRepository<Pessoa, Long> {
+
+    @Query(value = "select u from Pessoa u where u.nome like %?1%")
+    ArrayList<Pessoa> buscarPorNome(String nome);
+
+}
