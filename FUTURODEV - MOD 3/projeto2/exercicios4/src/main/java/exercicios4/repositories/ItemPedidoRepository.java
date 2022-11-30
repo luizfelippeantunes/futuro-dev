82a613cb5230539b8b51841ccd6f40d78fb0bc96
luizfelippeantunes <@NTUNEs31101989>
2022-11-29 21:49:38 -0300

    M3P2.1

--- FUTURODEV - MOD 3/projeto2/exercicios4/src/main/java/exercicios4/repositories/ItemPedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package exercicios4.repositories;
-
-import exercicios4.models.ItemPedido;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ItemPedidoRepository extends CrudRepository<ItemPedido, Long> {
-}
