96bba612c84a5b492bdea78addce4a46528ada1c
luizfelippeantunes <@NTUNEs31101989>
2022-12-02 21:02:41 -0300

    M3P2.4

--- FUTURODEV - MOD 3/projeto2/LabShoppingList/src/main/java/br/com/futurodev/labShoppingList/repository/FormaPagamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.futurodev.labShoppingList.repository;
-
-import br.com.futurodev.labShoppingList.model.FormaPagamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
-}
--- FUTURODEV - MOD 3/projeto2/projeto2 - Copia/src/main/java/projeto2/repositories/ex/ItemPedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package projeto2.repositories.ex;
-
-import projeto2.models.ex.ItemPedido;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ItemPedidoRepository extends CrudRepository<ItemPedido, Long> {
-}
